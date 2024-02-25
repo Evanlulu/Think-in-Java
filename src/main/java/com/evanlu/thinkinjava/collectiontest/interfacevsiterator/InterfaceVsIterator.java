66412fa9052713a8343f9d1626eda13fb8aa4d11
EvanLu <coffeelululu@gmail.com>
2024-02-25 13:10:39 +0800

    Think in Java p239

--- src/main/java/com/evanlu/thinkinjava/collectiontest/interfacevsiterator/InterfaceVsIterator.java
+++ src/main/java/com/evanlu/thinkinjava/collectiontest/interfacevsiterator/InterfaceVsIterator.java
@@ -1,9 +1,9 @@
 package com.evanlu.thinkinjava.collectiontest.interfacevsiterator;
 
 import com.evanlu.thinkinjava.pets.Pet;
+import com.evanlu.thinkinjava.pets.Pets;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class InterfaceVsIterator {
     public static void display(Iterator<Pet> it){
@@ -18,6 +18,25 @@ public class InterfaceVsIterator {
             System.out.println(p.id() + ":" + p + " ");
         }
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Pet> pets = Pets.arrayList(8);
+        HashSet<Pet> petSet = new HashSet<>(pets);
+        LinkedHashMap<String, Pet> pMap = new LinkedHashMap<>();
+        String[] names = ("Palph, Eric, Robin, Lacey, Britney, Sam, Spot, Fluffy").split(", ");
+        for (int i = 0; i < names.length; i++)
+            pMap.put(names[i], pets.get(i));
+        display(pets);
+        display(petSet);
+        display(pets.iterator());
+        display(petSet.iterator());
+        System.out.println(pMap);
+        System.out.println(pMap.keySet());
+        display(pMap.values());
+        display(pMap.values().iterator());
+
+
     }
 
 }
