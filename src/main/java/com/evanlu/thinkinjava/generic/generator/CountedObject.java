3aa1f4cccde105037d3f175ef6252daff057659f
INSYNERGER\evanlu <coffeelululu@gmail.com>
2024-03-08 10:32:47 +0800

    Think in Java p381

---
+++ src/main/java/com/evanlu/thinkinjava/generic/generator/CountedObject.java
@@ -0,0 +1,14 @@
+package com.evanlu.thinkinjava.generic.generator;
+
+public class CountedObject {
+    private static long counter = 0;
+    private final long id = counter++ ;
+    public long id(){ return id; }
+
+    @Override
+    public String toString() {
+        return "CountedObject{" +
+                "id=" + id +
+                '}';
+    }
+}
